fcaa8527dedc8dab436b4ae173b4be86f83c0a87
Phong Tran <thanhphongtran1703@gmail.com>
2025-08-20 12:38:12 +0000

    a bit of C++

---
+++ cpp28.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+void bakePizza();
+void bakePizza(std::string topping1);
+
+int main(){
+
+	bakePizza("asdjhf");
+
+	return 0;
+}
+void bakePizza(){
+
+	std::cout<< "Here is your Pizza!\n";
+
+}
+void bakePizza(std::string topping1){
+
+	std::cout<< "Here is your" << topping1 <<" pizza!\n";
+
+}
