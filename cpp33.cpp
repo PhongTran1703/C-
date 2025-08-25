3bc2d959128b17bc3ef4b5d82662e017b4fade8c
Phong Tran <thanhphongtran1703@gmail.com>
2025-08-25 12:28:36 +0000

    also array

---
+++ cpp33.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+	double gpa = 2.5;
+
+	std::string name = "Phong";
+	std::cout << sizeof(gpa) << " bytes\n";	
+	std::cout << sizeof(name)<< " bytes\n";
+
+
+	
+		
+		
+	return 0;
+}
