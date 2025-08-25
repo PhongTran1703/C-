5e152f5ae6701f53b77b22962e8840657b2269ec
Phong Tran <thanhphongtran1703@gmail.com>
2025-08-25 12:10:07 +0000

    practicing array

---
+++ cpp32.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+
+	std::string car [] = {"corvette","Mustang", "Camry"};
+
+	std::cout << car[0] << '\n';
+
+	double prices[]= {5.00,7.50,9.99,15.00};
+	std::cout << prices [0] << '\n';
+
+	std::cout << prices [1] << '\n';
+	std::cout << prices [2] << '\n';
+	std::cout << prices [3] << '\n';
+	return 0;
+}
