11331cf64099ca7336d336a61c37203ca89f017a
Phong Tran <thanhphongtran1703@gmail.com>
2025-08-25 12:46:40 +0000

    array

--- cpp33.cpp
+++ cpp33.cpp
@@ -2,14 +2,18 @@
 
 int main(){
 
-	double gpa = 2.5;
+	char grades[]= {'A', 'B', 'C', 'D', 'E', 'F','G'};
+	//double gpa = 2.5;
+	//std::string name = "Phong";
+	//std::cout << sizeof(gpa) << " bytes\n";	
+	//std::cout << sizeof(name)<< " bytes\n";
 
-	std::string name = "Phong";
-	std::cout << sizeof(gpa) << " bytes\n";	
-	std::cout << sizeof(name)<< " bytes\n";
-
-
+	std::cout << sizeof(grades)/sizeof(char)<< " elements\n";
 	
+	for(int i = 0; i < sizeof(grades)/sizeof(char); i++){
+		std::cout << grades[i]<< '\n';
+	
+	}
 		
 		
 	return 0;
