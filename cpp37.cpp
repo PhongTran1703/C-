5a51f0d7cacf5b6c324981e4a99194720e6f8738
Phong Tran <thanhphongtran1703@gmail.com>
2025-08-26 16:07:29 +0000

    bubble sort array

---
+++ cpp37.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+
+void sort(int array[], int size);
+
+
+int main() {
+
+	int array[]= {10, 4, 5, 8, 1, 3, 2, 9, 7,6};
+
+	int size = sizeof(array)/sizeof(int);
+
+	sort(array, size);
+
+
+        //std::cout << "The arry has " << size << " elements\n";
+
+	for(int element : array){
+	
+		std::cout << element << " ";
+	}
+
+	return 0;
+}
+
+
+void sort(int array[], int size){
+
+	int temp;
+	for(int i = 0; i < size - 1;i++){
+		for(int j = 0; j < size - i - 1;j++){
+
+			if(array[j] > array[j + 1]){
+			temp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = temp;
+			
+			}
+		}	
+	}
+
+
+
+}
