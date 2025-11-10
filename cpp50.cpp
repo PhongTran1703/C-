1042f66b29ad41030117aa9a48a5ec40f202ef39
Phong Tran <thanhphongtran1703@gmail.com>
2025-11-10 16:44:02 +0000

    upadate Dynamic memory

--- cpp50.cpp
+++ cpp50.cpp
@@ -2,7 +2,37 @@
 
 int main (){
 
+	char *pGrades = NULL;
+	int *pNum = NULL;
 
+	pNum = new int;
+	int size;
+
+	std::cout << "How many grades to enter in?: ";
+
+	std::cin >> size;
+	pGrades = new char[5];
+
+	*pNum = 21;
+
+	std::cout << "address: " << pNum << '\n';
+
+	std::cout << "value: " << *pNum << '\n';
+
+	for(int i = 0; i < size; i++){
+	
+		std::cout << "Enter grade #" << i + 1 << ": ";
+		std::cin >> pGrades[i];
+	}
+
+	for(int i = 0; i < size; i++){
+	
+		std::cout << pGrades[i] << " ";
+	}
+	std::cout << "\n";
+
+	delete pGrades;
+	delete pNum;
 
 	return 0;
 }
