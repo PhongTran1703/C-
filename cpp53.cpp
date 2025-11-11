a252a92f7e0b2775dc80e0a83d6426759389cdb0
Phong Tran <thanhphongtran1703@gmail.com>
2025-11-11 14:29:33 +0000

    Struct

---
+++ cpp53.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+struct student{
+
+	std::string name;
+	double gpa;
+	bool enrolled;
+};
+
+int main(){
+
+	student student1;
+	student1.name = "Tran";
+	student1.gpa = 4;
+	student1.enrolled = true;
+
+	std::cout << student1.name << '\n';
+	std::cout << student1.gpa << '\n';
+	std::cout << student1.enrolled << '\n';
+	return 0;
+}
